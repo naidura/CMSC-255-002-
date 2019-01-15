00e9d73a4b6173b23aef192a0c3a402824f59a6f
naidura <46724152+naidura@users.noreply.github.com>
2019-01-15 13:40:52 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+//Hello.Java
+/*Hello Printer
+Setting up the software for Java
+Rashmi Naidu
+1/15/2019
+CMSC 255-002*/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Rashmi Naidu");
+		System.out.println("Hello Printer");
+		System.out.println("creating first lab project in Java");
+		System.out.println("1" + "/" + "19");
+		System.out.println("CMSC255" + " 002");
+		System.out.println();
+	}
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
